20b66b9b62759c8d8569c9d4433296c13507a161
Spencer Chigananda <schigananda@bancabc.co.zw>
2021-03-15 20:21:06 +0200

    Initial commit

---
+++ src/main/java/com/spencerchigananda/financialplanningtool/FinancialPlanningToolApplication.java
@@ -0,0 +1,13 @@
+package com.spencerchigananda.financialplanningtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPlanningToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialPlanningToolApplication.class, args);
+	}
+
+}
